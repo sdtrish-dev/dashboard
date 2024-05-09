e6dbbb4b560d29251c9cba72e647ca2d54884c20
Trish Gill <95247759+sdtrish-dev@users.noreply.github.com>
2024-05-09 11:16:23 -0400

    [feat] add search to customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,29 @@
 import { Metadata } from 'next';
- 
+import { Suspense } from 'react';
+import CustomersTable from '@/app/ui/customers/table';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+
+  // Fetch customers based on the query
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <Suspense fallback={<InvoicesTableSkeleton />}>
+      <CustomersTable customers={customers} />
+    </Suspense>
+  );
 }
