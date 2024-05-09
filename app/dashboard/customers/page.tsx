61820755db3ea0ef0c1025bce58845566256e7b6
Trish Gill <95247759+sdtrish-dev@users.noreply.github.com>
2024-05-09 11:48:40 -0400

    [feat] add pagination to customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -2,7 +2,8 @@ import { Metadata } from 'next';
 import { Suspense } from 'react';
 import CustomersTable from '@/app/ui/customers/table';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
-import { fetchFilteredCustomers } from '@/app/lib/data';
+import { fetchFilteredCustomers, fetchInvoicesPages } from '@/app/lib/data';
+import Pagination from '@/app/ui/invoices/pagination';
 
 export const metadata: Metadata = {
   title: 'Customers',
@@ -17,13 +18,18 @@ export default async function Page({
   };
 }) {
   const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  const { customers, totalPages } = await fetchFilteredCustomers(query, currentPage);
 
-  // Fetch customers based on the query
-  const customers = await fetchFilteredCustomers(query);
 
   return (
+    <>
     <Suspense fallback={<InvoicesTableSkeleton />}>
       <CustomersTable customers={customers} />
     </Suspense>
+    <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+      </>
   );
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -186,32 +186,36 @@ export async function fetchCustomers() {
 
     const customers = data.rows;
     return customers;
+    
   } catch (err) {
     console.error('Database Error:', err);
     throw new Error('Failed to fetch all customers.');
   }
 }
 
-export async function fetchFilteredCustomers(query: string) {
+export async function fetchFilteredCustomers(query: string, page: number) {
   noStore();
+  const offset = (page - 1) * ITEMS_PER_PAGE;
+
   try {
     const data = await sql<CustomersTableType>`
-		SELECT
-		  customers.id,
-		  customers.name,
-		  customers.email,
-		  customers.image_url,
-		  COUNT(invoices.id) AS total_invoices,
-		  SUM(CASE WHEN invoices.status = 'pending' THEN invoices.amount ELSE 0 END) AS total_pending,
-		  SUM(CASE WHEN invoices.status = 'paid' THEN invoices.amount ELSE 0 END) AS total_paid
-		FROM customers
-		LEFT JOIN invoices ON customers.id = invoices.customer_id
-		WHERE
-		  customers.name ILIKE ${`%${query}%`} OR
+        SELECT
+          customers.id,
+          customers.name,
+          customers.email,
+          customers.image_url,
+          COUNT(invoices.id) AS total_invoices,
+          SUM(CASE WHEN invoices.status = 'pending' THEN invoices.amount ELSE 0 END) AS total_pending,
+          SUM(CASE WHEN invoices.status = 'paid' THEN invoices.amount ELSE 0 END) AS total_paid
+        FROM customers
+        LEFT JOIN invoices ON customers.id = invoices.customer_id
+        WHERE
+          customers.name ILIKE ${`%${query}%`} OR
         customers.email ILIKE ${`%${query}%`}
-		GROUP BY customers.id, customers.name, customers.email, customers.image_url
-		ORDER BY customers.name ASC
-	  `;
+        GROUP BY customers.id, customers.name, customers.email, customers.image_url
+        ORDER BY customers.name ASC
+        LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
+      `;
 
     const customers = data.rows.map((customer) => ({
       ...customer,
@@ -219,7 +223,17 @@ export async function fetchFilteredCustomers(query: string) {
       total_paid: formatCurrency(customer.total_paid),
     }));
 
-    return customers;
+    const totalCustomers = await sql<{ count: number }>`
+      SELECT COUNT(*) as count
+      FROM customers
+      WHERE
+        customers.name ILIKE ${`%${query}%`} OR
+        customers.email ILIKE ${`%${query}%`}
+    `;
+
+    const totalPages = Math.ceil(totalCustomers.rows[0].count / ITEMS_PER_PAGE);
+
+    return { customers, totalPages };
   } catch (err) {
     console.error('Database Error:', err);
     throw new Error('Failed to fetch customer table.');
